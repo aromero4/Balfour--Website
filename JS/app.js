67a4de96a9909ebf949e8b30d6b4cfc8d467c61c
Oliver Aceves <oaceves@nd.edu>
2019-07-24 00:16:29 -0400

    git commit

--- JS/app.js
+++ JS/app.js
@@ -1,3 +1,12 @@
+$('.smooth').on('click', function() {
+    $.smoothScroll({
+        scrollElement: $('body'),
+        scrollTarget: '#' + this.id
+    });
+    
+    return false;
+});
+
 $('.sidebar .toggle').on('click', function(e) {
     
     e.preventDefault();
